09cf245dca0c485d9222c4f14976927c9fa117bb
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-05-11 16:51:32 +0200

    He creado un regex helper con todas las regex necesarias para comprobar todos los text fields tanto del register como del login.

---
+++ app/src/main/java/com/example/musapp/feature/user/helper/RegexHelper.kt
@@ -0,0 +1,10 @@
+package com.example.musapp.feature.user.helper
+
+import android.util.Patterns
+
+object RegexHelper {
+    val nameRegex = Regex("^[A-ZÁÉÍÓÚÑ]{1}[a-záéíóúñ]+$")
+    val surnameRegex = Regex("^[A-ZÁÉÍÓÚÑ]{1}[a-záéíóúñ]+(\\s+[A-Z]{1}[a-z]+)?$")
+    val emailRegex = Regex(Patterns.EMAIL_ADDRESS.toString())
+    val passwordRegex = Regex("^[A-Za-z0-9*+-_]+$")
+}
