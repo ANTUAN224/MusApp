21eaab11c9e4aeca40b9c92a7b08dac8a532b594
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-05-17 14:17:21 +0200

    He añadido la dependencia necesaria para utilizar la librería de Testing MockK.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.room.runtime) //Dependencia para poder utilizar la librería Room para facilitar las operaciones CRUD en una BD SQLite.
     ksp(libs.androidx.room.compiler) //Dependencia para poder utilizar la herramienta KSP (Kotlin Symbol Processing) para las anotaciones de Room.
     implementation(libs.androidx.room.ktx) //Dependencia opcional que incluye funciones de extensión y soporte para corrutinas en DAO, simplificando el uso de Room.
+    testImplementation(libs.mockk) //Dependencia para poder utilizar la librería de testing MockK
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
