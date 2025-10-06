132d0ecae7a33f9ff62c500fa4781cd178832c4d
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-06 16:23:46 +0200

    He comentado el código de una función del servicio (que en realidad es cliente, está mal la nomenclatura) relacionado con la obtención de la información del perfil del usuario.

--- app/src/main/java/com/project/musapp/feature/home/user/profile/data/source/remote/service/UserProfileApiService.kt
+++ app/src/main/java/com/project/musapp/feature/home/user/profile/data/source/remote/service/UserProfileApiService.kt
@@ -1,11 +1,10 @@
 package com.project.musapp.feature.home.user.profile.data.source.remote.service
 
-import com.project.musapp.feature.user.register.data.model.remote.dto.RegisterUserRemoteDTO
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface UserProfileApiService {
-    @POST("users")
-    suspend fun insertUser(@Body registerUserRemoteDto: RegisterUserRemoteDTO) : Response<Void>
+//    @POST("users")
+//    suspend fun insertUser(@Body registerUserRemoteDto: RegisterUserDTO) : Response<Void>
 }
