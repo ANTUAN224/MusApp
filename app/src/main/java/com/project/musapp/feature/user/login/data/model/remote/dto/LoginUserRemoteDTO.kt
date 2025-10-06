1d7752d977fab92e789627cd318adc1ee1c2ecaf
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-06 16:17:37 +0200

    Se han eliminado los archivos DTO, DAO y de servicio relacionados con el inicio de sesión y el registro de usuarios.

--- app/src/main/java/com/project/musapp/feature/user/login/data/model/remote/dto/LoginUserRemoteDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.project.musapp.feature.user.login.data.model.remote.dto
-
-import com.project.musapp.feature.user.login.domain.model.LoginUserModel
-
-data class LoginUserRemoteDTO(val email: String, val password: String)
-
-fun LoginUserModel.toRemoteDTO() = LoginUserRemoteDTO(email = email, password = password)
