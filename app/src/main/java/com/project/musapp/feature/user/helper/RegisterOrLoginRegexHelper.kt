e94c24b7f8c9e96fede4cad0343a8c65571f39b4
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-09 14:08:17 +0200

    He añadido más nombres de dominio en la regex del email.

--- app/src/main/java/com/project/musapp/feature/user/helper/RegisterOrLoginRegexHelper.kt
+++ app/src/main/java/com/project/musapp/feature/user/helper/RegisterOrLoginRegexHelper.kt
@@ -2,6 +2,6 @@ package com.project.musapp.feature.user.helper
 
 object RegisterOrLoginRegexHelper {
     val nameOrSurnameRegex = Regex(pattern = "^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+\\s?([A-ZÁÉÍÓÚÑ][a-záéíóúñ]+\\s?)?$")
-    val emailRegex = Regex(pattern = "^[a-z0-9]+@(gmail|outlook)+\\.(com|es)\\s?$")
+    val emailRegex = Regex(pattern = "^[a-z0-9]+@(gmail|hotmail|yahoo)+\\.(com|es)\\s?$")
     val passwordRegex = Regex(pattern = "^[A-Za-z0-9*+-_#?@]+$")
 }
