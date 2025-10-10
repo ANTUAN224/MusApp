93bac3ead6a54d64d4e835d7a0e4e680036e542c
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-10 19:10:34 +0200

    He creado la data class del DTO del perfil del usuario (necesario en la solicitud POST de insertar usuario como repsuesta del servidor).

---
+++ app/src/main/java/com/project/musapp/feature/home/user/profile/data/model/dto/UserProfileDTO.kt
@@ -0,0 +1,8 @@
+package com.project.musapp.feature.home.user.profile.data.model.dto
+
+data class UserProfileDTO(
+    val name: String,
+    val surnames: String,
+    val birthdate : String,
+    val email : String
+)
