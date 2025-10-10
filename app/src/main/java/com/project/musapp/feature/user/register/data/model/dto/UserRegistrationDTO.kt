187f68eba5e019cd328068cef3d1d92bc11fbeb1
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-10 16:54:00 +0200

    He creado una data class que funciona como DTO del registro del usuario.

---
+++ app/src/main/java/com/project/musapp/feature/user/register/data/model/dto/UserRegistrationDTO.kt
@@ -0,0 +1,8 @@
+package com.project.musapp.feature.user.register.data.model.dto
+
+data class UserRegistrationDTO(
+    val name: String,
+    val surnames: String,
+    val birthdate: String,
+    val email: String
+)
