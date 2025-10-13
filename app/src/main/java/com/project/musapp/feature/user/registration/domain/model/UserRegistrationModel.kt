aa921b2b6012af09574f4622934ce035d866ea1a
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-13 14:15:56 +0200

    He añadido un atributo que contiene la ruta local de la imagen del perfil del usuario, ya sea la de por defecto o la seleccionada desde el launcher.

--- app/src/main/java/com/project/musapp/feature/user/registration/domain/model/UserRegistrationModel.kt
+++ app/src/main/java/com/project/musapp/feature/user/registration/domain/model/UserRegistrationModel.kt
@@ -1,18 +1,21 @@
 package com.project.musapp.feature.user.registration.domain.model
 
+import android.net.Uri
 import com.project.musapp.feature.user.registration.data.model.dto.UserRegistrationDTO
 
 class UserRegistrationModel(
     val name: String,
     val surnames: String,
     val birthdateText: String,
-    val email: String
+    val email: String,
+    val profileImageLocalPath: Uri
 )
 
-fun UserRegistrationModel.toDTO(): UserRegistrationDTO =
+fun UserRegistrationModel.toDTO(firebaseStorageProfileImageUrl: String): UserRegistrationDTO =
     UserRegistrationDTO(
         name = name,
         surnames = surnames,
         birthdate = birthdateText,
-        email = email
+        email = email,
+        firebaseStorageProfileImageUrl = firebaseStorageProfileImageUrl
     )
