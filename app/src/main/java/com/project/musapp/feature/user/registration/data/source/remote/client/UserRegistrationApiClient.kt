2a98f351dc2e21862f327b074b19be77d7135919
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-14 16:35:06 +0200

    He cambiado la respuesta de la función asociada con solicitud HTTP POST de crear un nuevo usuario, haciendo que no tenga cuerpo.

--- app/src/main/java/com/project/musapp/feature/user/registration/data/source/remote/client/UserRegistrationApiClient.kt
+++ app/src/main/java/com/project/musapp/feature/user/registration/data/source/remote/client/UserRegistrationApiClient.kt
@@ -1,6 +1,5 @@
 package com.project.musapp.feature.user.registration.data.source.remote.client
 
-import com.project.musapp.feature.home.user.profile.data.model.dto.UserProfileDTO
 import com.project.musapp.feature.user.registration.data.model.dto.UserRegistrationDTO
 import retrofit2.Response
 import retrofit2.http.Body
@@ -12,5 +11,5 @@ interface UserRegistrationApiClient {
     suspend fun insertUser(
         @Header("Authorization") headerCompanionValue: String,
         @Body userRegistrationDTO: UserRegistrationDTO
-    ): Response<UserProfileDTO>
+    ): Response<Void>
 }
