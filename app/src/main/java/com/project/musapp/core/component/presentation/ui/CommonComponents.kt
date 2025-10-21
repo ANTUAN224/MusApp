075ddc6bc555d96099cbe1daeba7d63764e33780
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-21 12:51:06 +0200

    He movido archivo que contiene los componentes que se utilizan en toda la app para que esté mejor organizado el proyecto.

--- app/src/main/java/com/project/musapp/core/component/presentation/ui/CommonComponents.kt
+++ app/src/main/java/com/project/musapp/core/component/presentation/ui/CommonComponents.kt
@@ -1,4 +1,4 @@
-package com.project.musapp.core.common.components.presentation.ui
+package com.project.musapp.core.component.presentation.ui
 
 import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.size
