819d89f2fa3b73aef588509a070121b1505728db
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-21 12:16:00 +0200

    He renombrado el archivo que contiene los componentes comunes en toda la app. Además, he renombrado la fun composable que contiene el circular progress indicator. Además, he renombrado y traído la fun composable que contiene el modal avisando de la no conexión a Internet.

---
+++ app/src/main/java/com/project/musapp/core/common/components/presentation/ui/CommonComponents.kt
@@ -0,0 +1,33 @@
+package com.project.musapp.core.common.components.presentation.ui
+
+import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CommonCircularProgressIndicator() {
+    CircularProgressIndicator(
+        modifier = Modifier.size(60.dp),
+        color = Color(color = 0xFF12AA7A),
+        trackColor = Color.LightGray,
+        strokeWidth = 5.dp
+    )
+}
+
+@Composable
+fun CommonNoInternetConnectionModal() {
+    val activity = LocalActivity.current!!
+    AlertDialog(
+        onDismissRequest = { activity.finish() },
+        confirmButton = { TextButton(onClick = { activity.finish() }) { Text(text = "Cerrar") } },
+        title = { Text(text = "Sin conexión a Internet") },
+        text = { Text(text = "Actualmente, no tienes acceso a Internet, por lo que no podrás acceder a la app. Verifica tu conexión y vuelva a intentarlo en otra ocasión.") }
+    )
+}
