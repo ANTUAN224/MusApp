5029cfa23e2d84137d088fe4bbe28834c3567b61
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-22 20:10:59 +0200

    He cambiado el tipo de retorno en la función 'verifyUserLogin' a Unit para que no devuelva nada tanto en la interfaz del repositorio como en su implementación.

--- app/src/main/java/com/project/musapp/feature/user/auth/login/data/repository/UserLoginRepositoryImp.kt
+++ app/src/main/java/com/project/musapp/feature/user/auth/login/data/repository/UserLoginRepositoryImp.kt
@@ -1,10 +1,10 @@
 package com.project.musapp.feature.user.auth.login.data.repository
 
-import com.project.musapp.feature.user.auth.login.data.source.remote.UserLoginRemoteDataSource
+import com.project.musapp.feature.user.auth.login.data.source.remote.UserLoginFirebaseAuth
 import com.project.musapp.feature.user.auth.login.domain.repository.UserLoginRepository
 import javax.inject.Inject
 
-class UserLoginRepositoryImp @Inject constructor(private val remoteDataSource: UserLoginRemoteDataSource) :
+class UserLoginRepositoryImp @Inject constructor(private val remoteDataSource: UserLoginFirebaseAuth) :
     UserLoginRepository {
     override suspend fun verifyUserLogin(email: String, password: String) =
         remoteDataSource.verifyUserLogin(email = email, password = password)
--- app/src/main/java/com/project/musapp/feature/user/auth/login/domain/repository/UserLoginRepository.kt
+++ app/src/main/java/com/project/musapp/feature/user/auth/login/domain/repository/UserLoginRepository.kt
@@ -1,5 +1,5 @@
 package com.project.musapp.feature.user.auth.login.domain.repository
 
 interface UserLoginRepository {
-    suspend fun verifyUserLogin(email: String, password: String): Boolean
+    suspend fun verifyUserLogin(email: String, password: String)
 }
