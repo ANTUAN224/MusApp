9f97f0f563c6d122ef5d364bdeaf1fb821c89200
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-22 20:01:04 +0200

    He eliminado dos archivos que no necesito en la feature.

--- app/src/main/java/com/project/musapp/feature/user/auth/login/data/model/dto/LoginUserDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.project.musapp.feature.user.auth.login.data.model.dto
-
-import com.project.musapp.feature.user.auth.login.domain.model.LoginUserModel
-
-data class LoginUserDTO(val email: String, val password: String)
-
-fun LoginUserModel.toRemoteDTO() = LoginUserDTO(email = email, password = password)
