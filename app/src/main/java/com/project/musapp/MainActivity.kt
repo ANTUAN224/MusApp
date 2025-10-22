56212eb1a87117e89236cfd973d30cb7309d6704
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-22 18:31:22 +0200

    Se han actualizado varias rutas dadas las actualizaciones de ubicación de packages mencionadas. Además, he actualizado nombres y parámetros de funs composables y he añadido la val booleana que indica si mostrar o no el modal de ausencia de conexión a Internet.

--- app/src/main/java/com/project/musapp/MainActivity.kt
+++ app/src/main/java/com/project/musapp/MainActivity.kt
@@ -15,16 +15,16 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.FirebaseApp
-import com.project.musapp.core.feature.navigation.item.presentation.ui.MusAppNavigationBar
-import com.project.musapp.core.feature.navigation.item.presentation.viewmodel.NavigationViewModel
-import com.project.musapp.core.feature.navigation.routing.RouteHub
-import com.project.musapp.feature.user.initialchecking.presentation.ui.UserInitialCheckingScreen
-import com.project.musapp.feature.user.initialchecking.presentation.viewModel.UserStateInitialCheckingViewModel
-import com.project.musapp.feature.user.initialmenu.presentation.ui.InitialMenuScreen
-import com.project.musapp.feature.user.login.presentation.ui.UserLoginModal
-import com.project.musapp.feature.user.login.presentation.viewmodel.UserLoginViewModel
-import com.project.musapp.feature.user.registration.presentation.ui.UserRegistrationScreen
-import com.project.musapp.feature.user.registration.presentation.viewmodel.UserRegistrationViewModel
+import com.project.musapp.core.navigation.item.presentation.ui.MusAppNavigationBar
+import com.project.musapp.core.navigation.item.presentation.viewmodel.NavigationViewModel
+import com.project.musapp.core.navigation.routing.RouteHub
+import com.project.musapp.feature.user.auth.login.presentation.ui.UserLoginModal
+import com.project.musapp.feature.user.auth.login.presentation.viewmodel.UserLoginViewModel
+import com.project.musapp.feature.user.auth.registration.presentation.ui.UserRegistrationScreen
+import com.project.musapp.feature.user.auth.registration.presentation.viewmodel.UserRegistrationViewModel
+import com.project.musapp.initialchecking.presentation.ui.UserStateInitialCheckingScreen
+import com.project.musapp.initialchecking.presentation.viewModel.UserStateInitialCheckingViewModel
+import com.project.musapp.feature.initialmenu.presentation.ui.InitialMenuScreen
 import com.project.musapp.ui.theme.MusAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlin.getValue
@@ -99,7 +99,7 @@ class MainActivity : ComponentActivity() {
                                 }
                             }
 
-                            UserInitialCheckingScreen(
+                            UserStateInitialCheckingScreen(
                                 hasInternetConnection = hasInternetConnection
                             )
                         }
@@ -137,17 +137,24 @@ class MainActivity : ComponentActivity() {
                             val userRegistrationViewModel: UserRegistrationViewModel =
                                 hiltViewModel(viewModelStoreOwner = navBackStackEntry)
 
-                            val isLoading by userRegistrationViewModel.isLoading.observeAsState(initial = false)
+                            val isLoading by userRegistrationViewModel.isLoading.observeAsState(
+                                initial = false
+                            )
 
-                            val navigateToHome by userRegistrationViewModel.navigateToHome.observeAsState(initial = null)
+                            val navigateToHome by userRegistrationViewModel.navigateToHome.observeAsState(
+                                initial = null
+                            )
+
+                            val showNoInternetConnectionModal by userRegistrationViewModel.showNoInternetConnectionModal.observeAsState(
+                                initial = false
+                            )
 
                             UserRegistrationScreen(
                                 viewModel = userRegistrationViewModel,
                                 context = applicationContext,
-                                topBarTitle = "Registro de usuario",
-                                bodyTitle = "Introduce tus datos",
                                 isLoading = isLoading,
                                 navigateToHome = navigateToHome,
+                                showNoInternetConnectionModal = showNoInternetConnectionModal,
                                 onReturnButtonPress = { navController.popBackStack() },
                                 onRegisterButtonPress = {
                                     userRegistrationViewModel.onUserRegistrationScreenButtonPress()
@@ -170,7 +177,11 @@ class MainActivity : ComponentActivity() {
 
                         composable<RouteHub.Home> {
                             LaunchedEffect(Unit) {
-                                Toast.makeText(applicationContext, "Bienvenido a casita", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Bienvenido a casita",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         }
                     }
