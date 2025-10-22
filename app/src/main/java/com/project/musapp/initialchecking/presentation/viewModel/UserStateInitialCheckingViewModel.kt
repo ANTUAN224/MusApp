2edb39c94a7eebf878d35876ef9b696ac8f2cf61
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-22 18:21:49 +0200

    He movido los archivos relacionados con el chequeo inicial de conexión a Internet y estado de sesión del usuario fuera del package 'feature' dado que no lo es.

---
+++ app/src/main/java/com/project/musapp/initialchecking/presentation/viewModel/UserStateInitialCheckingViewModel.kt
@@ -0,0 +1,40 @@
+package com.project.musapp.initialchecking.presentation.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.project.musapp.core.internetconnectionverification.domain.exception.NoInternetConnectionException
+import com.project.musapp.core.internetconnectionverification.domain.usecase.VerifyUserInternetConnectionUseCase
+import com.project.musapp.core.sessionstateverification.domain.usecase.VerifyUserSessionStateUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UserStateInitialCheckingViewModel
+@Inject constructor(
+    private val verifyUserInternetConnectionUseCase: VerifyUserInternetConnectionUseCase,
+    private val verifyUserSessionStateUseCase: VerifyUserSessionStateUseCase
+) : ViewModel() {
+    private val _hasInternetConnection = MutableLiveData<Boolean?>()
+    val hasInternetConnection: LiveData<Boolean?> = _hasInternetConnection
+
+    private val _hasActiveSession = MutableLiveData<Boolean?>()
+    val hasActiveSession: LiveData<Boolean?> = _hasActiveSession
+
+    fun onUserInitialChecking() {
+        viewModelScope.launch {
+            delay(2000) //Simula el tiempo de carga inicial
+
+            try {
+                verifyUserInternetConnectionUseCase()
+                _hasInternetConnection.value = true
+                _hasActiveSession.value = verifyUserSessionStateUseCase()
+            } catch (_: NoInternetConnectionException) {
+                _hasInternetConnection.value = false
+            }
+        }
+    }
+}
