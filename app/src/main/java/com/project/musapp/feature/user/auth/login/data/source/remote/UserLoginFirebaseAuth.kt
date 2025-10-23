7cec061a53bf256aec558c3ffe9fc51e1cb27999
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-10-23 19:00:47 +0200

    He añadido un timeout en la fun que se encarga del login del usuario en la app dado que el FirebaseNetworkException no siempre se lanza cuando se produce una desconexión en la operación.

--- app/src/main/java/com/project/musapp/feature/user/auth/login/data/source/remote/UserLoginFirebaseAuth.kt
+++ app/src/main/java/com/project/musapp/feature/user/auth/login/data/source/remote/UserLoginFirebaseAuth.kt
@@ -1,22 +1,33 @@
 package com.project.musapp.feature.user.auth.login.data.source.remote
 
+import android.util.Log
 import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.project.musapp.core.internetconnectionverification.domain.exception.NoInternetConnectionException
 import com.project.musapp.feature.user.auth.login.domain.exception.UserNotFoundException
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withTimeout
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
 class UserLoginFirebaseAuth @Inject constructor() {
     suspend fun verifyUserLogin(email: String, password: String) {
+        val timeout = 5.seconds
+
         try {
-            Firebase.auth.signInWithEmailAndPassword(email, password).await()
-        } catch (_: FirebaseNetworkException) {
-            throw NoInternetConnectionException()
-        } catch (_: FirebaseAuthException) {
-            throw UserNotFoundException()
+            withTimeout(timeout = timeout) {
+                Log.d("NETWORK_EXCEPTION", "Verificando credenciales del usuario...")
+                Firebase.auth.signInWithEmailAndPassword(email, password).await()
+                Log.d("NETWORK_EXCEPTION", "Credenciales del usuario verificadas.")
+            }
+        } catch (e: Exception) {
+            when (e) {
+                is FirebaseNetworkException, is TimeoutCancellationException -> throw NoInternetConnectionException()
+                is FirebaseAuthException -> throw UserNotFoundException()
+            }
         }
     }
 }
