6616d31f6c648a761a11efe1d001e62927954825
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-03 10:05:58 +0100

    He cambiado el constructor de la data class para que se pueda alternar entre imageVector y painter en relación a la representación de iconos de las secciones de la barra de navegación.

---
+++ app/src/main/java/com/project/musapp/core/navigation/presentation/model/NavItem.kt
@@ -0,0 +1,10 @@
+package com.project.musapp.core.navigation.presentation.model
+
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavItem(
+    val label: String,
+    val imageVector: ImageVector? = null,
+    val painter: Painter? = null,
+    val contentDescription: String)
--- app/src/main/java/com/project/musapp/core/navigation/presentation/ui/NavigationUI.kt
+++ app/src/main/java/com/project/musapp/core/navigation/presentation/ui/NavigationUI.kt
@@ -3,7 +3,6 @@ package com.project.musapp.core.navigation.presentation.ui
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Folder
-import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.School
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -12,15 +11,30 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import com.project.musapp.core.navigation.domain.model.NavItem
+import androidx.compose.ui.res.painterResource
+import com.project.musapp.R
+import com.project.musapp.core.navigation.presentation.model.NavItem
 import com.project.musapp.core.navigation.presentation.viewmodel.NavigationViewModel
 
 @Composable
 fun MusAppNavigationBar(navigationViewModel: NavigationViewModel, navItemIndex: Int) {
     val navItemList = listOf(
-        NavItem(label = "Home", icon = Icons.Outlined.Home),
-        NavItem(label = "Colecciones", icon = Icons.Outlined.Folder),
-        NavItem(label = "Cultura artística", icon = Icons.Outlined.School)
+        NavItem(
+            label = "Home",
+            painter = painterResource(R.drawable.home_24px),
+            contentDescription = "Pantalla principal"
+        ),
+        NavItem(
+            label = "Colecciones",
+            imageVector = Icons.Outlined.Folder,
+            contentDescription = "Apartado de colecciones creadas por el usuario"
+        ),
+        NavItem(
+            label = "Cultura artística",
+            imageVector = Icons.Outlined.School,
+            contentDescription = "Apartado de glosario técnico relacionado con el arte plástico y curiosidades " +
+                    "de los cuadros y artistas"
+        )
     )
     NavigationBar(containerColor = Color(color = 0xFF12AA7A)) {
         navItemList.forEachIndexed { index, item ->
@@ -40,7 +54,15 @@ fun RowScope.MusAppNavigationItem(
 ) {
     NavigationBarItem(
         label = { Text(text = navItem.label) },
-        icon = { Icon(imageVector = navItem.icon, contentDescription = null) },
+        icon = {
+            if (navItem.imageVector != null) Icon(
+                imageVector = navItem.imageVector,
+                contentDescription = navItem.contentDescription
+            ) else Icon(
+                painter = navItem.painter!!,
+                contentDescription = navItem.contentDescription
+            )
+        },
         selected = isSelected,
         onClick = { onNavItemClick() },
         colors = NavigationBarItemDefaults.colors(
