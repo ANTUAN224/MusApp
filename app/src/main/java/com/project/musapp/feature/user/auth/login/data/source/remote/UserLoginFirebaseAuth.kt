4bdf136adf80395da7b5e7f759964de4ee40d37e
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-06 00:24:38 +0100

    He movido el try-catch del data source a la implementación del repositorio.

--- app/src/main/java/com/project/musapp/feature/user/auth/login/data/source/remote/UserLoginFirebaseAuth.kt
+++ app/src/main/java/com/project/musapp/feature/user/auth/login/data/source/remote/UserLoginFirebaseAuth.kt
@@ -1,12 +1,7 @@
 package com.project.musapp.feature.user.auth.login.data.source.remote
 
-import com.google.firebase.FirebaseNetworkException
-import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.project.musapp.core.internetconnectionverification.domain.exception.NoInternetConnectionException
-import com.project.musapp.feature.user.auth.login.domain.exception.UserNotFoundException
-import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withTimeout
 import javax.inject.Inject
@@ -16,15 +11,8 @@ class UserLoginFirebaseAuth @Inject constructor() {
     suspend fun logInUser(email: String, password: String) {
         val timeout = 5.seconds
 
-        try {
-            withTimeout(timeout = timeout) {
-                Firebase.auth.signInWithEmailAndPassword(email, password).await()
-            }
-        } catch (e: Exception) {
-            when (e) {
-                is FirebaseNetworkException, is TimeoutCancellationException -> throw NoInternetConnectionException()
-                is FirebaseAuthException -> throw UserNotFoundException()
-            }
+        withTimeout(timeout = timeout) {
+            Firebase.auth.signInWithEmailAndPassword(email, password).await()
         }
     }
 }
