0d5cf6c6967c5ab95cfb9f02be85b35dccb4f3dc
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-06 00:20:21 +0100

    He movido el try-catch del data source a la implementación del repositorio.

--- app/src/main/java/com/project/musapp/core/tokengetting/data/repository/UserTokenGettingRepositoryImp.kt
+++ app/src/main/java/com/project/musapp/core/tokengetting/data/repository/UserTokenGettingRepositoryImp.kt
@@ -1,5 +1,7 @@
 package com.project.musapp.core.tokengetting.data.repository
 
+import com.google.firebase.FirebaseNetworkException
+import com.project.musapp.core.internetconnectionverification.domain.exception.InternetConnectionVerificationException
 import com.project.musapp.core.tokengetting.data.source.remote.UserTokenGettingFirebaseAuth
 import com.project.musapp.core.tokengetting.domain.repository.UserTokenGettingRepository
 import javax.inject.Inject
@@ -7,6 +9,11 @@ import javax.inject.Inject
 class UserTokenGettingRepositoryImp @Inject constructor(
     private val userTokenGettingFirebaseAuth: UserTokenGettingFirebaseAuth
 ) : UserTokenGettingRepository {
-    override suspend fun getUserToken() =
-        this.userTokenGettingFirebaseAuth.getFirebaseUserToken()
+    override suspend fun getUserToken(): String {
+        try {
+            return this.userTokenGettingFirebaseAuth.getFirebaseUserToken()
+        } catch (_: FirebaseNetworkException) {
+            throw InternetConnectionVerificationException.NoInternetConnectionException
+        }
+    }
 }
