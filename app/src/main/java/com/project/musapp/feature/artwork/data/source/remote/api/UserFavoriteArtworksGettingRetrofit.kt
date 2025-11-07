a123c67baa0b80c18de18bd7b2597e5434e354b9
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-07 18:05:33 +0100

    He cambiado la dependencia que recibe por el constructor ya que no era la correcta.

--- app/src/main/java/com/project/musapp/feature/artwork/data/source/remote/api/UserFavoriteArtworksGettingRetrofit.kt
+++ app/src/main/java/com/project/musapp/feature/artwork/data/source/remote/api/UserFavoriteArtworksGettingRetrofit.kt
@@ -1,14 +1,13 @@
 package com.project.musapp.feature.artwork.data.source.remote.api
 
 import com.project.musapp.feature.artwork.data.model.dto.artwork.ArtworkPreviewDTO
-import com.project.musapp.feature.profile.data.source.remote.api.UserProfileApiService
 import javax.inject.Inject
 
 class UserFavoriteArtworksGettingRetrofit @Inject constructor(
-    private val userProfileApiService: UserProfileApiService
+    private val artworkApiService: ArtworkApiService
 ) {
     suspend fun getUserFavoriteArtworks(userToken: String): List<ArtworkPreviewDTO> {
-        return userProfileApiService.getUserFavoriteArtworks(headerCompanionValue = "Bearer $userToken")
+        return artworkApiService.getUserFavoriteArtworks(headerCompanionValue = "Bearer $userToken")
             .body()!!
     }
 }
