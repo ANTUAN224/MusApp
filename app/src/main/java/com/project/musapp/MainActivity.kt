5dac063a597828afff228aafc057afeb31e74612
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-11 20:35:05 +0100

    He eliminado imports que no se estaban utilizando.

--- app/src/main/java/com/project/musapp/MainActivity.kt
+++ app/src/main/java/com/project/musapp/MainActivity.kt
@@ -4,35 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.Composable
 import com.google.firebase.FirebaseApp
-import com.project.musapp.feature.auth.presentation.login.ui.LoginModal
-import com.project.musapp.feature.auth.presentation.login.ui.UserNotFoundModal
-import com.project.musapp.feature.auth.presentation.login.viewmodel.UserLoginViewModel
-import com.project.musapp.feature.auth.presentation.registration.ui.EmailAlreadyInUseModal
-import com.project.musapp.feature.auth.presentation.registration.ui.UserRegistrationScreen
-import com.project.musapp.feature.auth.presentation.registration.viewmodel.UserRegistrationViewModel
-import com.project.musapp.ui.commoncomponents.CommonLoadingScreen
-import com.project.musapp.ui.commoncomponents.CommonNoInternetConnectionModal
-import com.project.musapp.navigation.presentation.navigationbar.ui.MusAppNavigationBar
-import com.project.musapp.navigation.presentation.navigationbar.viewmodel.NavigationViewModel
-import com.project.musapp.navigation.routing.RouteHub
-import com.project.musapp.feature.home.presentation.ui.HomeScreen
-import com.project.musapp.feature.home.presentation.viewmodel.HomeViewModel
 import com.project.musapp.navigation.presentation.entrypoint.NavigationEntryPoint
-import com.project.musapp.navigation.presentation.initialmenu.ui.InitialMenuScreen
-import com.project.musapp.navigation.presentation.splashscreen.viewModel.SplashScreenViewModel
 import com.project.musapp.ui.theme.MusAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.getValue
 
 @AndroidEntryPoint //Permite que se puedan inyectar dependencias dentro de la clase.
 class MainActivity : ComponentActivity() {
