929b584e10d30ec6d611a5cae7bcbad05b76d2cd
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-13 17:23:11 +0100

    He eliminado el try-catch dado que la conversión a la excepción de dominio la realiza la implementación del repositorio.

--- app/src/main/java/com/project/musapp/feature/artwork/data/source/remote/api/ArtworkGettingRetrofit.kt
+++ app/src/main/java/com/project/musapp/feature/artwork/data/source/remote/api/ArtworkGettingRetrofit.kt
@@ -1,21 +1,18 @@
 package com.project.musapp.feature.artwork.data.source.remote.api
 
-import com.project.musapp.core.network.domain.exception.NetworkException
-import com.project.musapp.feature.artwork.data.model.dto.artwork.ArtworkDTO
-import java.io.IOException
 import javax.inject.Inject
 
 class ArtworkGettingRetrofit @Inject constructor(
     private val artworkApiService: ArtworkApiService
 ) {
-    suspend fun getArtwork(userToken: String, artworkId: Long): ArtworkDTO {
-        try {
-            return artworkApiService.getArtwork(
-                headerCompanionValue = "Bearer $userToken",
-                artworkId = artworkId
-            ).body()!!
-        } catch (_: IOException) {
-            throw NetworkException.NoInternetConnectionException
-        }
-    }
+    suspend fun getArtwork(userToken: String, artworkId: Long) =
+        artworkApiService.getArtwork(
+            headerCompanionValue = "Bearer $userToken",
+            artworkId = artworkId
+        ).body()!!
+
+    suspend fun getSearchArtworks(userToken: String) =
+        artworkApiService.getSearchArtworks(
+            headerCompanionValue = "Bearer $userToken"
+        ).body()!!
 }
