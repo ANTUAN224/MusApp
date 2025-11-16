9e491a8cb83d9e22be02f83dd7a75bc0642d4e31
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-16 17:28:01 +0100

    He cambiado el contenido de la fun para añadir el listener que ejecuta la lambda cuando Firebase Auth ha terminado de realizar sus operaciones asíncronas.

--- app/src/main/java/com/project/musapp/feature/auth/data/source/local/UserSessionStateVerificationFirebaseAuth.kt
+++ app/src/main/java/com/project/musapp/feature/auth/data/source/local/UserSessionStateVerificationFirebaseAuth.kt
@@ -1,9 +1,20 @@
 package com.project.musapp.feature.auth.data.source.local
 
+import android.util.Log
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
 
 class UserSessionStateVerificationFirebaseAuth @Inject constructor() {
-    fun verifyUserSession() = Firebase.auth.currentUser != null
+    fun verifyUserSession(): Boolean {
+        Log.d("EJECUCIÓN", "Usuario actual (inicialmente) -> ${Firebase.auth.currentUser}")
+        var isUserLoggedIn = false
+
+        Firebase.auth.addAuthStateListener { firebaseAuth ->
+            isUserLoggedIn = firebaseAuth.currentUser != null
+            Log.d("EJECUCIÓN", "Usuario actual (finalmente) -> ${Firebase.auth.currentUser}")
+        }
+
+        return isUserLoggedIn
+    }
 }
