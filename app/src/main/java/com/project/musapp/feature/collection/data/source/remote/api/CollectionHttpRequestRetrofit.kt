df23c20718d2b30a0dc01aaee76460bd1c742def
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-19 15:23:48 +0100

    He creado la clase que interactúa con la api service para realizar las operaciones CRUD de collections.

---
+++ app/src/main/java/com/project/musapp/feature/collection/data/source/remote/api/CollectionHttpRequestRetrofit.kt
@@ -0,0 +1,39 @@
+package com.project.musapp.feature.collection.data.source.remote.api
+
+import com.project.musapp.feature.collection.data.model.dto.CollectionBatchDeletionDTO
+import com.project.musapp.feature.collection.data.model.dto.CollectionCreationDTO
+import com.project.musapp.feature.collection.data.model.dto.CollectionRenamingDTO
+import javax.inject.Inject
+
+class CollectionHttpRequestRetrofit @Inject constructor(
+    private val collectionApiService: CollectionApiService
+) {
+    suspend fun createCollection(
+        userToken: String,
+        collectionCreationDTO: CollectionCreationDTO
+    )  =
+        collectionApiService.createCollection(
+            headerCompanionValue = "Bearer $userToken",
+            collectionCreationDTO = collectionCreationDTO
+        ).body()!!
+
+    suspend fun renameCollection(
+        userToken: String,
+        collectionId: Long,
+        collectionRenamingDTO: CollectionRenamingDTO
+    ) =
+        collectionApiService.renameCollection(
+            headerCompanionValue = "Bearer $userToken",
+            collectionId = collectionId,
+            collectionRenamingDTO = collectionRenamingDTO
+        ).body()!!
+
+    suspend fun deleteCollections(
+        userToken: String,
+        collectionBatchDeletionDTO: CollectionBatchDeletionDTO
+    ) =
+        collectionApiService.deleteCollections(
+            headerCompanionValue = "Bearer $userToken",
+            collectionBatchDeletionDTO = collectionBatchDeletionDTO
+        ).body()!!
+}
