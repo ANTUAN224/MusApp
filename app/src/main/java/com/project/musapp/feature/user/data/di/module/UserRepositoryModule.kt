45c7d45340bc71d8d8eb7674c38c7ee1b32f3677
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-19 15:21:36 +0100

    He cambiado el scope de la implementación del repositorio a Singleton dado que se usa en casos de uso inyectados en varios View Models.

--- app/src/main/java/com/project/musapp/feature/user/data/di/module/UserRepositoryModule.kt
+++ app/src/main/java/com/project/musapp/feature/user/data/di/module/UserRepositoryModule.kt
@@ -7,11 +7,13 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ViewModelComponent::class) //Indica que la dependencia generada tendrá el scope del ViewModel de Home.
+@InstallIn(SingletonComponent::class)
 abstract class UserRepositoryModule {
     @Binds
-    @ViewModelScoped //Se crea una instancia singleton para el scope del ViewModel del Home.
-    abstract fun bind(homeRepositoryImp: UserRepositoryImp) : UserRepository
+    @Singleton
+    abstract fun bind(userRepositoryImp: UserRepositoryImp) : UserRepository
 }
