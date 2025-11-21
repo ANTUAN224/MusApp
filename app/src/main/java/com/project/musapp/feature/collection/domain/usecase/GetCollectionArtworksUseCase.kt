b5a7e593566cd57a8678bd0eba8a8f1754641a97
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-21 10:27:01 +0100

    He creado el caso de uso que se encarga de llamar a la fun del repositorio de Collection encargada de obtener los cuadros de una colección del usuario.

---
+++ app/src/main/java/com/project/musapp/feature/collection/domain/usecase/GetCollectionArtworksUseCase.kt
@@ -0,0 +1,21 @@
+package com.project.musapp.feature.collection.domain.usecase
+
+import com.project.musapp.feature.artwork.domain.model.artwork.toUiModel
+import com.project.musapp.feature.artwork.presentation.model.artwork.ArtworkPreviewUiModel
+import com.project.musapp.feature.auth.domain.usecase.GetUserTokenUseCase
+import com.project.musapp.feature.collection.domain.repository.CollectionRepository
+import javax.inject.Inject
+
+class GetCollectionArtworksUseCase @Inject constructor(
+    private val repository: CollectionRepository,
+    private val getUserTokenUseCase: GetUserTokenUseCase
+) {
+    suspend operator fun invoke(collectionId: Long): Result<List<ArtworkPreviewUiModel>> {
+        return runCatching {
+            repository.getCollectionArtworks(
+                userToken = getUserTokenUseCase().getOrThrow(),
+                collectionId = collectionId
+            ).map { artworkPreviewDomainModel -> artworkPreviewDomainModel.toUiModel() }
+        }
+    }
+}
