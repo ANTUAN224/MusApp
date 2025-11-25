a2a04bdb71c8c2797a1b091b7e8cbeb36bcf3253
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-11-25 17:51:03 +0100

    He creado los atributos y funciones necesarias para gestionar la navegación y estados relacionados con la adición a favoritos de un cuadro y su adición y eliminación de colecciones.

--- app/src/main/java/com/project/musapp/navigation/presentation/navigationbar/viewmodel/NavigationViewModel.kt
+++ app/src/main/java/com/project/musapp/navigation/presentation/navigationbar/viewmodel/NavigationViewModel.kt
@@ -28,6 +28,16 @@ class NavigationViewModel @Inject constructor() : ViewModel() {
     val hasArtworkBeenNavigatedFromCollection: LiveData<Boolean> =
         _hasArtworkBeenNavigatedFromCollection
 
+    private val _hasArtworkBeenAddedToCollections = MutableLiveData<Boolean>()
+    val hasArtworkBeenAddedToCollections: LiveData<Boolean> = _hasArtworkBeenAddedToCollections
+
+    private val _hasArtworkBeenDeletedFromCollections = MutableLiveData<Boolean>()
+    val hasArtworkBeenDeletedFromCollections: LiveData<Boolean> =
+        _hasArtworkBeenDeletedFromCollections
+
+    private val _hasArtworkBeenMarkedAsFavorite = MutableLiveData<Boolean>()
+    val hasArtworkBeenMarkedAsFavorite: LiveData<Boolean> = _hasArtworkBeenMarkedAsFavorite
+
     fun onNavBarShowing() {
         _showNavBar.value = true
     }
@@ -54,12 +64,40 @@ class NavigationViewModel @Inject constructor() : ViewModel() {
         _hasArtworkBeenNavigatedFromCollection.value = true
     }
 
+    fun onReturnToCollectionPreviews() {
+        _hasArtworkBeenNavigatedFromCollection.value = false
+    }
+
     fun onCollectionArrival() {
         viewModelScope.launch {
             delay(3000)
 
             _hasArtworkBeenNavigatedFromCollection.value = false
         }
+    }
+
+    fun onArtworkMarkedAsFavoriteStateChange(hasArtworkBeenMarkedAsFavorite: Boolean) {
+        _hasArtworkBeenMarkedAsFavorite.value = hasArtworkBeenMarkedAsFavorite
+    }
+
+    fun onArtworkAdditionToCollections() {
+        _hasArtworkBeenAddedToCollections.value = true
+    }
+
+    fun onHomeArrival() {
+        if (hasArtworkBeenMarkedAsFavorite.value != null) {
+            _hasArtworkBeenMarkedAsFavorite.value = null
+        }
+
+        if (hasArtworkBeenAddedToCollections.value ?: false) {
+            _hasArtworkBeenAddedToCollections.value = false
+        } else if (hasArtworkBeenDeletedFromCollections.value ?: false) {
+            _hasArtworkBeenDeletedFromCollections.value = false
+        }
+    }
+
+    fun onArtworkDeletionFromCollections() {
+        _hasArtworkBeenDeletedFromCollections.value = true
     }
 
     fun onArtisticCultureFirstTimeArrival() {
